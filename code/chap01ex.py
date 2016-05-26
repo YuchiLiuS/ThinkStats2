a443d1fa5287ee7b03adb214150732faf1cd5ca7
YuchiLiuS <lychpkuphy@gmail.com>
2016-05-26 00:09:15 -0700

    chap01 exercise

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,12 +13,41 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz'):
+    """Reads the NSFG pregnancy data.
 
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    return df
+
+def ValidatePregNum(resp):
+    fem_preg = nsfg.ReadFemPreg()
+    preg_map = nsfg.MakePregMap(fem_preg)
+    
+    for index, pregnum in resp.pregnum.iteritems():
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+        if len(indices) != pregnum:
+            print (caseid, len(indices), pregnum)
+            return False
+    return True
+    
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+    df = ReadFemResp()
+    assert(len(df) == 7643)
+    assert(df.pregnum.value_counts()[0] == 2610)
+    print(df.pregnum.value_counts().sort_index())
+    assert(ValidatePregNum(df))
     print('%s: All tests passed.' % script)
 
 
